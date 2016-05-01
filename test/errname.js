b2a3b2b8e291d633323add38485e901ef2a2a52e
James Talmage <james@talmage.io>
2016-05-01 19:46:59 -0400

    fix Windows failure on Node 0.12

--- test/errname.js
+++ test/errname.js
@@ -1,8 +1,8 @@
-import os from 'os';
 import test from 'ava';
 import errname from '../lib/errname';
 
-const isWin = os.platform() === 'win32';
+const isWin = process.platform === 'win32';
+const majorVersion = Number(process.version.substr(1).split('.')[0]);
 
 // simulates failure to capture process.binding('uv');
 function fallback(code) {
@@ -10,16 +10,21 @@ function fallback(code) {
 }
 
 function makeTests(name, m, expected) {
-	test(name, t => {
+	test(`${name}: >=0 exit codes`, t => {
 		// throws >= 0
 		t.throws(() => m(0), /err >= 0/);
 		t.throws(() => m(1), /err >= 0/);
 		t.throws(() => m('2'), /err >= 0/);
 		t.throws(() => m('foo'), /err >= 0/);
-
-		t.is(m(-2), expected);
-		t.is(m('-2'), expected);
 	});
+
+	if (!(isWin && majorVersion < 1)) {
+		// causes process.exit() on Node 0.12 for Windows.
+		test(`${name}: negative exit codes`, t => {
+			t.is(m(-2), expected);
+			t.is(m('-2'), expected);
+		});
+	}
 }
 
 const unknown = 'Unknown system error -2';

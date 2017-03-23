7e1d17b6270a3240fd4cc81de4987df4ab31fcb0
Sam Verschueren <sam.verschueren@gmail.com>
2017-03-23 07:46:14 +0100

    add fd test

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 import path from 'path';
+import fs from 'fs';
 import stream from 'stream';
 import childProcess from 'child_process';
 import test from 'ava';
 import getStream from 'get-stream';
 import isRunning from 'is-running';
 import delay from 'delay';
+import tempfile from 'tempfile';
 import m from './';
 
 process.env.PATH = path.join(__dirname, 'fixtures') + path.delimiter + process.env.PATH;
@@ -67,6 +69,18 @@ test('do not include `stderr` in errors when set to `inherit`', async t => {
 	const err = await t.throws(m('fixtures/error-message.js', {stderr: 'inherit'}));
 	t.regex(err.message, /stdout/);
 	t.notRegex(err.message, /stderr/);
+});
+
+test('pass `stdout` to a file descriptor', async t => {
+	const file = tempfile('.txt');
+	await m('fixtures/noop', ['foo bar'], {stdout: fs.openSync(file, 'w')});
+	t.is(fs.readFileSync(file, 'utf8'), 'foo bar\n');
+});
+
+test('pass `stderr` to a file descriptor', async t => {
+	const file = tempfile('.txt');
+	await m('fixtures/noop-err', ['foo bar'], {stderr: fs.openSync(file, 'w')});
+	t.is(fs.readFileSync(file, 'utf8'), 'foo bar\n');
 });
 
 test('execa.shell()', async t => {

f185a0e8bee153a67dc92a8477b33a659cc5274c
Sindre Sorhus <sindresorhus@gmail.com>
2018-11-12 23:19:16 +0700

    Fix tests

--- test/errname.js
+++ test/errname.js
@@ -9,10 +9,10 @@ const fallback = code => errname.__test__(null, code);
 function makeTests(name, m, expected) {
 	test(`${name}: >=0 exit codes`, t => {
 		// Throws >= 0
-		t.throws(() => m(0), /err >= 0|It must be a negative integer/);
-		t.throws(() => m(1), /err >= 0|It must be a negative integer/);
-		t.throws(() => m('2'), /err >= 0|must be of type number/);
-		t.throws(() => m('foo'), /err >= 0|must be of type number/);
+		t.throws(() => m(0), /err >= 0|It must be a negative integer|must be of type negative number/);
+		t.throws(() => m(1), /err >= 0|It must be a negative integer|must be of type negative number/);
+		t.throws(() => m('2'), /err >= 0|must be of type number|must be of type negative number/);
+		t.throws(() => m('foo'), /err >= 0|must be of type number|must be of type negative number/);
 	});
 
 	test(`${name}: negative exit codes`, t => {

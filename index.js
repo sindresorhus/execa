43d7163cc8139826cd5efe1c6c4b43929b08b8f1
ehmicky <ehmicky@gmail.com>
2019-03-15 15:08:33 +0100

    Fix opts.input

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function handleArgs(command, args, options) {
 }
 
 function handleInput(spawned, input) {
-	if (input === undefined) {
+	if (input === null || input === undefined) {
 		return;
 	}
 

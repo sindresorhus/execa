93b8063257bfb79477a3505eb117f5b790a6c666
Philipp Tusch zokker13@posteo.de <zokker13@posteo.de>
2019-04-28 13:19:56 +0200

    Add feature to retry process killing after specified duration
    Implements #105

--- index.js
+++ index.js
@@ -221,6 +221,28 @@ const execa = (file, args, options) => {
 		return Promise.reject(error);
 	}
 
+	const originalKill = spawned.kill.bind(spawned);
+	spawned.kill = (signal, options) => {
+		if (signal === undefined || signal === 'SIGTERM') {
+			const retryAfter = options !== undefined &&
+				['number', 'string'].includes(typeof options.retryAfter)
+				? Number(options.retryAfter)
+				: 2000;
+
+				setTimeout(() => {
+					try {
+						originalKill(0);
+						originalKill('SIGKILL');
+					} catch (err) {
+						// All good - process killed
+					}
+				}, retryAfter);
+				originalKill(signal);
+		} else {
+			originalKill(signal);
+		}
+	};
+
 	// #115
 	let removeExitHandler;
 	if (parsed.options.cleanup && !parsed.options.detached) {

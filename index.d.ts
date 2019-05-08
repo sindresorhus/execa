b7d1d0ce4d65ea24836c9fa44a0a7b9096730a52
ehmicky <ehmicky@gmail.com>
2019-05-08 12:12:54 +0200

    Fix TypeScript comment

--- index.d.ts
+++ index.d.ts
@@ -65,7 +65,11 @@ declare namespace execa {
 
 		/**
 		If `true`, runs `command` inside of a shell. Uses `/bin/sh` on UNIX and `cmd.exe` on Windows. A different shell can be specified as a string. The shell should understand the `-c` switch on UNIX or `/d /s /c` on Windows.
-		We recommend against using this option since it is not cross-platform, slower and unsafe.
+
+		We recommend against using this option since it is:
+		- not cross-platform, encouraging shell-specific syntax.
+		- slower, because of the additional shell interpretation.
+		- unsafe, potentially allowing command injection.
 
 		@default false
 		*/

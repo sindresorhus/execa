72a99220af01f959a3af464ad96f31e894e118e2
ehmicky <ehmicky@gmail.com>
2019-05-14 22:02:48 +0200

    Fix Windows tests again

--- test.js
+++ test.js
@@ -65,14 +65,14 @@ test('stdout/stderr/all are undefined if ignored in sync mode', t => {
 	t.is(all, undefined);
 });
 
-const WRONG_COMMAND_STDOUT = process.platform === 'win32' ?
+const WRONG_COMMAND_STDERR = process.platform === 'win32' ?
 	'\'wrong\' is not recognized as an internal or external command,\r\noperable program or batch file.' :
 	'';
 
 test('stdout/stderr/all on process errors', async t => {
 	const {stdout, stderr, all} = await t.throwsAsync(execa('wrong command'));
-	t.is(stdout, WRONG_COMMAND_STDOUT);
-	t.is(stderr, '');
+	t.is(stdout, '');
+	t.is(stderr, WRONG_COMMAND_STDERR);
 	t.is(all, '');
 });
 
@@ -80,8 +80,8 @@ test('stdout/stderr/all on process errors, in sync mode', t => {
 	const {stdout, stderr, all} = t.throws(() => {
 		execa.sync('wrong command');
 	});
-	t.is(stdout, WRONG_COMMAND_STDOUT);
-	t.is(stderr, '');
+	t.is(stdout, '');
+	t.is(stderr, WRONG_COMMAND_STDERR);
 	t.is(all, undefined);
 });
 

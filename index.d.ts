842a8d9c757460aac351ac6dc7abe9368e17233e
ehmicky <ehmicky@users.noreply.github.com>
2019-11-24 15:01:03 +0100

    Update index.d.ts
    Co-Authored-By: Sindre Sorhus <sindresorhus@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -137,7 +137,7 @@ declare namespace execa {
 
 		/**
 		When using the `stdio: 'ipc'` option or `execa.node()`, how messages passed to [`childProcess.send()`](https://nodejs.org/api/child_process.html#child_process_subprocess_send_message_sendhandle_options_callback) are serialized:
-			- `json`: uses `JSON.serialize()`
+			- `json`: Uses `JSON.stringify()` and `JSON.parse()`.
 			- `advanced`: Uses [`v8.serialize()`](https://nodejs.org/api/v8.html#v8_v8_serialize_value)
 
 		[More info.](https://nodejs.org/api/child_process.html#child_process_advanced_serialization).

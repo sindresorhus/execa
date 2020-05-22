d692e1c4ee7ee415c53de748dd0abbcb4c58005a
Sindre Sorhus <sindresorhus@gmail.com>
2020-05-22 21:50:02 +0800

    Update override-promise.js

--- test/override-promise.js
+++ test/override-promise.js
@@ -3,8 +3,8 @@ import test from 'ava';
 
 process.env.PATH = path.join(__dirname, 'fixtures') + path.delimiter + process.env.PATH;
 
-// Can't use `test.before`, maybe because `ava` need `Promise`
-// Can't use `import('..')` too, because `execa` is not ES Module
+// Can't use `test.before`, because `ava` needs `Promise`.
+// Can't use `import(…)` either, because `execa` is not an ES Module.
 const nativePromise = Promise;
 global.Promise = class BrokenPromise {
 	then() {
@@ -14,7 +14,7 @@ global.Promise = class BrokenPromise {
 const execa = require('..');
 global.Promise = nativePromise;
 
-test('Should work with third party Promise', async t => {
+test('should work with third-party Promise', async t => {
 	const {stdout} = await execa('noop', ['foo']);
 	t.is(stdout, 'foo');
 });

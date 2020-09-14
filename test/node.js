027910d12e2b35afa5f9d8182bfc8933446e0832
Ward Peeters <ward@coding-tech.com>
2020-09-14 12:20:14 +0200

    fix: remove inspect & inspect-brk from execArgv

--- test/node.js
+++ test/node.js
@@ -3,7 +3,8 @@ import test from 'ava';
 import pEvent from 'p-event';
 import execa from '..';
 
-process.env.PATH = path.join(__dirname, 'fixtures') + path.delimiter + process.env.PATH;
+process.env.PATH =
+	path.join(__dirname, 'fixtures') + path.delimiter + process.env.PATH;
 
 test('node()', async t => {
 	const {exitCode} = await execa.node('test/fixtures/noop');
@@ -19,11 +20,14 @@ test('node pipe stdout', async t => {
 });
 
 test('node correctly use nodePath', async t => {
-	const {stdout} = await execa.node(process.platform === 'win32' ? 'hello.cmd' : 'hello.sh', {
-		stdout: 'pipe',
-		nodePath: process.platform === 'win32' ? 'cmd.exe' : 'bash',
-		nodeOptions: process.platform === 'win32' ? ['/c'] : []
-	});
+	const {stdout} = await execa.node(
+		process.platform === 'win32' ? 'hello.cmd' : 'hello.sh',
+		{
+			stdout: 'pipe',
+			nodePath: process.platform === 'win32' ? 'cmd.exe' : 'bash',
+			nodeOptions: process.platform === 'win32' ? ['/c'] : []
+		}
+	);
 
 	t.is(stdout, 'Hello World');
 });
@@ -36,6 +40,94 @@ test('node pass on nodeOptions', async t => {
 
 	t.is(stdout, 'foo');
 });
+
+test.serial(
+	'node removes --inspect from nodeOptions when defined by parent process',
+	async t => {
+		const originalArgv = process.execArgv;
+		process.execArgv = ['--inspect', '-e'];
+		const {stdout, stderr} = await execa.node('console.log("foo")', {
+			stdout: 'pipe'
+		});
+		process.execArgv = originalArgv;
+
+		t.is(stdout, 'foo');
+		t.is(stderr, '');
+	}
+);
+
+test.serial(
+	'node removes --inspect=9222 from nodeOptions when defined by parent process',
+	async t => {
+		const originalArgv = process.execArgv;
+		process.execArgv = ['--inspect=9222', '-e'];
+		const {stdout, stderr} = await execa.node('console.log("foo")', {
+			stdout: 'pipe'
+		});
+		process.execArgv = originalArgv;
+
+		t.is(stdout, 'foo');
+		t.is(stderr, '');
+	}
+);
+
+test.serial(
+	'node removes --inspect-brk from nodeOptions when defined by parent process',
+	async t => {
+		const originalArgv = process.execArgv;
+		process.execArgv = ['--inspect-brk', '-e'];
+		const childProc = execa.node('console.log("foo")', {
+			stdout: 'pipe'
+		});
+		process.execArgv = originalArgv;
+
+		setTimeout(() => {
+			childProc.cancel();
+		}, 1000);
+
+		const {stdout, stderr} = await childProc.catch(error => error);
+
+		t.is(stdout, 'foo');
+		t.is(stderr, '');
+	}
+);
+
+test.serial(
+	'node removes --inspect-brk=9222 from nodeOptions when defined by parent process',
+	async t => {
+		const originalArgv = process.execArgv;
+		process.execArgv = ['--inspect-brk=9222', '-e'];
+		const childProc = execa.node('console.log("foo")', {
+			stdout: 'pipe'
+		});
+		process.execArgv = originalArgv;
+
+		setTimeout(() => {
+			childProc.cancel();
+		}, 1000);
+
+		const {stdout, stderr} = await childProc.catch(error => error);
+
+		t.is(stdout, 'foo');
+		t.is(stderr, '');
+	}
+);
+
+test.serial(
+	'node should not remove --inspect when passed through nodeOptions',
+	async t => {
+		const {stdout, stderr} = await execa.node('console.log("foo")', {
+			stdout: 'pipe',
+			nodeOptions: ['--inspect', '-e']
+		});
+
+		t.is(stdout, 'foo');
+		t.regex(
+			stderr,
+			/^Debugger listening on ws:\/\/127.0.0.1:9229\/.*[\r\n]+For help, see: https:\/\/nodejs.org\/en\/docs\/inspector$/
+		);
+	}
+);
 
 test('node\'s forked script has a communication channel', async t => {
 	const subprocess = execa.node('test/fixtures/send');

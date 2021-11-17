aefa85ce20840e6d6e26cd50f0700070dab22826
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2021-11-17 03:39:42 +0000

    Add Node.js 12 back to the test matrix

--- test/override-promise.js
+++ test/override-promise.js
@@ -2,20 +2,15 @@ import path from 'node:path';
 import process from 'node:process';
 import {fileURLToPath} from 'node:url';
 import test from 'ava';
+// The helper module overrides Promise on import so has to be imported before `execa`.
+// Can't use top-level await (TLA) + `import(…)` since Node.js 12 doesn't support TLA.
+import {restorePromise} from './helpers/override-promise.js';
+// eslint-disable-next-line import/order
+import {execa} from '../index.js';
+
+restorePromise();
 
 process.env.PATH = fileURLToPath(new URL('./fixtures', import.meta.url)) + path.delimiter + process.env.PATH;
-
-// Can't use `test.before`, because `ava` needs `Promise`.
-const nativePromise = Promise;
-global.Promise = class BrokenPromise {
-	then() {
-		throw new Error('error');
-	}
-};
-// eslint-disable-next-line node/no-unsupported-features/es-syntax
-const {execa} = await import('../index.js');
-
-global.Promise = nativePromise;
 
 test('should work with third-party Promise', async t => {
 	const {stdout} = await execa('noop.js', ['foo']);

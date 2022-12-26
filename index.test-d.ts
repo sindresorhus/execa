1e2824466ad1aa552097dadef02362f070987792
Emily M Klassen <forivall@gmail.com>
2022-12-26 11:41:09 -0700

    Fix test-d style

--- index.test-d.ts
+++ index.test-d.ts
@@ -210,7 +210,12 @@ expectType<ExecaReturnValue>(
 expectType<ExecaReturnValue<Buffer>>(
 	await execaNode('unicorns', ['foo'], {encoding: null}),
 );
-execaNode('unicorns', {nodeOptions: ['--async-stack-traces']})
-execaNode('unicorns', ['foo'], {nodeOptions: ['--async-stack-traces']})
-execaNode('unicorns', {nodeOptions: ['--async-stack-traces'], encoding: null})
-execaNode('unicorns', ['foo'], {nodeOptions: ['--async-stack-traces'], encoding: null})
+
+expectType<ExecaChildProcess>(execaNode('unicorns', {nodeOptions: ['--async-stack-traces']}));
+expectType<ExecaChildProcess>(execaNode('unicorns', ['foo'], {nodeOptions: ['--async-stack-traces']}));
+expectType<ExecaChildProcess<Buffer>>(
+	execaNode('unicorns', {nodeOptions: ['--async-stack-traces'], encoding: null}),
+);
+expectType<ExecaChildProcess<Buffer>>(
+	execaNode('unicorns', ['foo'], {nodeOptions: ['--async-stack-traces'], encoding: null}),
+);

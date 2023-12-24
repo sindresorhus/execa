8281f988dc2c8bd376934da2dd818ee12760321e
ehmicky <ehmicky@gmail.com>
2023-12-24 11:24:31 +0000

    Improve cleanup

--- test/fixtures/stdin-fd3.js
+++ test/fixtures/stdin-fd3.js
@@ -2,5 +2,5 @@
 import process from 'node:process';
 import {readFileSync} from 'node:fs';
 
-const fileDescriptorIndex = Number(process.argv[3] || 3);
+const fileDescriptorIndex = Number(process.argv[2] || 3);
 console.log(readFileSync(fileDescriptorIndex, {encoding: 'utf8'}));

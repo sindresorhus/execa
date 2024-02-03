75919056dfb6330ebcda48b22ba56cb05cc33358
ehmicky <ehmicky@users.noreply.github.com>
2024-02-03 05:20:50 +0000

    Add more error handling related tests (#773)

--- test/stdio/array.js
+++ test/stdio/array.js
@@ -251,6 +251,15 @@ test('Does not destroy process.stdin on child process errors', testDestroyStanda
 test('Does not destroy process.stdout on child process errors', testDestroyStandard, 1);
 test('Does not destroy process.stderr on child process errors', testDestroyStandard, 2);
 
+const testDestroyStandardSpawn = async (t, index) => {
+	await t.throwsAsync(execa('forever.js', {...getStdio(index, [STANDARD_STREAMS[index], 'pipe']), uid: -1}));
+	t.false(STANDARD_STREAMS[index].destroyed);
+};
+
+test('Does not destroy process.stdin on spawn process errors', testDestroyStandardSpawn, 0);
+test('Does not destroy process.stdout on spawn process errors', testDestroyStandardSpawn, 1);
+test('Does not destroy process.stderr on spawn process errors', testDestroyStandardSpawn, 2);
+
 const testDestroyStandardStream = async (t, index) => {
 	const childProcess = execa('forever.js', getStdio(index, [STANDARD_STREAMS[index], 'pipe']));
 	const error = new Error('test');

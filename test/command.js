b460f5593c110090710d435ddcf3a6808dbc18b3
ehmicky <ehmicky@gmail.com>
2024-02-18 13:54:08 +0000

    Improve input validation

--- test/command.js
+++ test/command.js
@@ -38,3 +38,23 @@ test('execaCommandSync()', t => {
 	const {stdout} = execaCommandSync('echo.js foo bar');
 	t.is(stdout, 'foo\nbar');
 });
+
+const testInvalidCommand = (t, execaCommand, invalidArgument) => {
+	t.throws(() => {
+		execaCommand(invalidArgument);
+	}, {message: /First argument must be a string/});
+};
+
+test('execaCommand() must use a string', testInvalidCommand, execaCommand, true);
+test('execaCommandSync() must use a string', testInvalidCommand, execaCommandSync, true);
+test('execaCommand() must have an argument', testInvalidCommand, execaCommand, undefined);
+test('execaCommandSync() must have an argument', testInvalidCommand, execaCommandSync, undefined);
+
+const testInvalidArgs = (t, execaCommand) => {
+	t.throws(() => {
+		execaCommand('echo', ['']);
+	}, {message: /Last argument must be an options object/});
+};
+
+test('execaCommand() must not pass an array of arguments', testInvalidArgs, execaCommand);
+test('execaCommandSync() must not pass an array of arguments', testInvalidArgs, execaCommandSync);

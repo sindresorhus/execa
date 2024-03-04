5abc429233bd8ed71a2dd992a60fa11a74653554
ehmicky <ehmicky@users.noreply.github.com>
2024-03-04 08:03:53 +0000

    Improve streams pipe logic (#885)

--- test/helpers/stream.js
+++ test/helpers/stream.js
@@ -1,7 +1,7 @@
-import {Readable, Writable, Duplex} from 'node:stream';
+import {Readable, Writable, PassThrough} from 'node:stream';
 import {foobarString} from './input.js';
 
 export const noopReadable = () => new Readable({read() {}});
 export const noopWritable = () => new Writable({write() {}});
-export const noopDuplex = () => new Duplex({read() {}, write() {}});
+export const noopDuplex = () => new PassThrough().resume();
 export const simpleReadable = () => Readable.from([foobarString]);

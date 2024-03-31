f0acd7d6abc077a92cae218fbe34b0456fa37941
ehmicky <ehmicky@gmail.com>
2024-03-31 11:24:08 +0100

    Fix `stdio: [..., undefined]` option

--- test/stdio/option.js
+++ test/stdio/option.js
@@ -21,21 +21,33 @@ stdioMacro.title = macroTitle('execa()');
 test(stdioMacro, {stdio: 'inherit'}, ['inherit', 'inherit', 'inherit']);
 test(stdioMacro, {stdio: 'pipe'}, ['pipe', 'pipe', 'pipe']);
 test(stdioMacro, {stdio: 'ignore'}, ['ignore', 'ignore', 'ignore']);
-test(stdioMacro, {stdio: [0, 1, 2]}, [0, 1, 2]);
 
-test(stdioMacro, {}, [undefined, undefined, undefined]);
-test(stdioMacro, {stdio: []}, [undefined, undefined, undefined]);
-test(stdioMacro, {stdin: 'pipe'}, ['pipe', undefined, undefined]);
-test(stdioMacro, {stdout: 'ignore'}, [undefined, 'ignore', undefined]);
-test(stdioMacro, {stderr: 'inherit'}, [undefined, undefined, 'inherit']);
+test(stdioMacro, {}, ['pipe', 'pipe', 'pipe']);
+test(stdioMacro, {stdio: []}, ['pipe', 'pipe', 'pipe']);
+test(stdioMacro, {stdio: [0]}, [0, 'pipe', 'pipe']);
+test(stdioMacro, {stdio: [0, 1]}, [0, 1, 'pipe']);
+test(stdioMacro, {stdio: [0, 1, 2]}, [0, 1, 2]);
+test(stdioMacro, {stdio: [0, 1, 2, 3]}, [0, 1, 2, 3]);
+test(stdioMacro, {stdio: [undefined, 1, 2]}, ['pipe', 1, 2]);
+test(stdioMacro, {stdio: [null, 1, 2]}, ['pipe', 1, 2]);
+test(stdioMacro, {stdio: [0, undefined, 2]}, [0, 'pipe', 2]);
+test(stdioMacro, {stdio: [0, null, 2]}, [0, 'pipe', 2]);
+test(stdioMacro, {stdio: [0, 1, undefined]}, [0, 1, 'pipe']);
+test(stdioMacro, {stdio: [0, 1, null]}, [0, 1, 'pipe']);
+test(stdioMacro, {stdio: [0, 1, 2, undefined]}, [0, 1, 2, 'ignore']);
+test(stdioMacro, {stdio: [0, 1, 2, null]}, [0, 1, 2, 'ignore']);
+
+test(stdioMacro, {stdin: 'pipe'}, ['pipe', 'pipe', 'pipe']);
+test(stdioMacro, {stdout: 'ignore'}, ['pipe', 'ignore', 'pipe']);
+test(stdioMacro, {stderr: 'inherit'}, ['pipe', 'pipe', 'inherit']);
 test(stdioMacro, {stdin: 'pipe', stdout: 'ignore', stderr: 'inherit'}, ['pipe', 'ignore', 'inherit']);
-test(stdioMacro, {stdin: 'pipe', stdout: 'ignore'}, ['pipe', 'ignore', undefined]);
-test(stdioMacro, {stdin: 'pipe', stderr: 'inherit'}, ['pipe', undefined, 'inherit']);
-test(stdioMacro, {stdout: 'ignore', stderr: 'inherit'}, [undefined, 'ignore', 'inherit']);
+test(stdioMacro, {stdin: 'pipe', stdout: 'ignore'}, ['pipe', 'ignore', 'pipe']);
+test(stdioMacro, {stdin: 'pipe', stderr: 'inherit'}, ['pipe', 'pipe', 'inherit']);
+test(stdioMacro, {stdout: 'ignore', stderr: 'inherit'}, ['pipe', 'ignore', 'inherit']);
 test(stdioMacro, {stdin: 0, stdout: 1, stderr: 2}, [0, 1, 2]);
-test(stdioMacro, {stdin: 0, stdout: 1}, [0, 1, undefined]);
-test(stdioMacro, {stdin: 0, stderr: 2}, [0, undefined, 2]);
-test(stdioMacro, {stdout: 1, stderr: 2}, [undefined, 1, 2]);
+test(stdioMacro, {stdin: 0, stdout: 1}, [0, 1, 'pipe']);
+test(stdioMacro, {stdin: 0, stderr: 2}, [0, 'pipe', 2]);
+test(stdioMacro, {stdout: 1, stderr: 2}, ['pipe', 1, 2]);
 
 test(stdioMacro, {stdio: {foo: 'bar'}}, new TypeError('Expected `stdio` to be of type `string` or `Array`, got `object`'));
 

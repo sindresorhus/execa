96d2ef7f9e3d01ea9fc968da49883c860d0e0dfe
ehmicky <ehmicky@users.noreply.github.com>
2024-04-02 18:11:32 +0100

    Fix `stdio: [..., undefined]` option (#948)

--- test/stdio/forward.js
+++ test/stdio/forward.js
@@ -2,15 +2,40 @@ import test from 'ava';
 import {execa} from '../../index.js';
 import {getStdio} from '../helpers/stdio.js';
 import {setFixtureDir} from '../helpers/fixtures-dir.js';
+import {foobarString, foobarUint8Array} from '../helpers/input.js';
 
 setFixtureDir();
 
-const testOverlapped = async (t, fdNumber) => {
-	const {stdout} = await execa('noop.js', ['foobar'], getStdio(fdNumber, ['overlapped', 'pipe']));
-	t.is(stdout, 'foobar');
+const testInputOverlapped = async (t, fdNumber) => {
+	const {stdout} = await execa('stdin-fd.js', [`${fdNumber}`, foobarString], getStdio(fdNumber, [foobarUint8Array, 'overlapped', 'pipe']));
+	t.is(stdout, foobarString);
 };
 
-test('stdin can be ["overlapped", "pipe"]', testOverlapped, 0);
-test('stdout can be ["overlapped", "pipe"]', testOverlapped, 1);
-test('stderr can be ["overlapped", "pipe"]', testOverlapped, 2);
-test('stdio[*] can be ["overlapped", "pipe"]', testOverlapped, 3);
+test('stdin can be ["overlapped", "pipe"]', testInputOverlapped, 0);
+test('stdio[*] input can be ["overlapped", "pipe"]', testInputOverlapped, 3);
+
+const testOutputOverlapped = async (t, fdNumber) => {
+	const {stdio} = await execa('noop-fd.js', [`${fdNumber}`, foobarString], getStdio(fdNumber, ['overlapped', 'pipe']));
+	t.is(stdio[fdNumber], foobarString);
+};
+
+test('stdout can be ["overlapped", "pipe"]', testOutputOverlapped, 1);
+test('stderr can be ["overlapped", "pipe"]', testOutputOverlapped, 2);
+test('stdio[*] output can be ["overlapped", "pipe"]', testOutputOverlapped, 3);
+
+const testFd3Undefined = async (t, stdioOption, options) => {
+	const subprocess = execa('empty.js', {...getStdio(3, stdioOption), ...options});
+	t.is(subprocess.stdio.length, 4);
+	t.is(subprocess.stdio[3], null);
+
+	const {stdio} = await subprocess;
+	t.is(stdio.length, 4);
+	t.is(stdio[3], undefined);
+};
+
+test('stdio[*] undefined means "ignore"', testFd3Undefined, undefined, {});
+test('stdio[*] null means "ignore"', testFd3Undefined, null, {});
+test('stdio[*] undefined means "ignore", "lines: true"', testFd3Undefined, undefined, {lines: true});
+test('stdio[*] null means "ignore", "lines: true"', testFd3Undefined, null, {lines: true});
+test('stdio[*] undefined means "ignore", "encoding: hex"', testFd3Undefined, undefined, {encoding: 'hex'});
+test('stdio[*] null means "ignore", "encoding: hex"', testFd3Undefined, null, {encoding: 'hex'});

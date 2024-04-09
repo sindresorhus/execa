1e9e47eb7bc9850df8f31f392e03d72252773d39
ehmicky <ehmicky@users.noreply.github.com>
2024-04-09 16:21:34 +0100

    Improve documentation of transform options (#969)

--- index.d.ts
+++ index.d.ts
@@ -44,11 +44,35 @@ type GeneratorFinal<IsSync extends boolean> = () =>
 | Unless<IsSync, AsyncGenerator<unknown, void, void>>
 | Generator<unknown, void, void>;
 
+/**
+A transform or an array of transforms can be passed to the `stdin`, `stdout`, `stderr` or `stdio` option.
+
+A transform is either a generator function or a plain object with the following members.
+*/
 type GeneratorTransformFull<IsSync extends boolean> = {
+	/**
+	Map or filter the input or output of the subprocess.
+	*/
 	transform: GeneratorTransform<IsSync>;
+
+	/**
+	Create additional lines after the last one.
+	*/
 	final?: GeneratorFinal<IsSync>;
+
+	/**
+	If `true`, iterate over arbitrary chunks of `Uint8Array`s instead of line `string`s.
+	*/
 	binary?: boolean;
+
+	/**
+	If `true`, keep newlines in each `line` argument. Also, this allows multiple `yield`s to produces a single line.
+	*/
 	preserveNewlines?: boolean;
+
+	/**
+	If `true`, allow `transformOptions.transform` and `transformOptions.final` to return any type, not just `string` or `Uint8Array`.
+	*/
 	objectMode?: boolean;
 };
 

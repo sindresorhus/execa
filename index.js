4b2e316e6860d27392c429311d55b16429fbbc0c
ehmicky <ehmicky@users.noreply.github.com>
2024-04-16 18:28:07 +0100

    Rename files (#977)

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import {createExeca} from './lib/arguments/create.js';
-import {mapCommandAsync, mapCommandSync} from './lib/command.js';
-import {mapNode} from './lib/arguments/node.js';
-import {mapScriptAsync, setScriptSync, deepScriptOptions} from './lib/script.js';
+import {createExeca} from './lib/methods/create.js';
+import {mapCommandAsync, mapCommandSync} from './lib/methods/command.js';
+import {mapNode} from './lib/methods/node.js';
+import {mapScriptAsync, setScriptSync, deepScriptOptions} from './lib/methods/script.js';
 
-export {ExecaError, ExecaSyncError} from './lib/return/cause.js';
+export {ExecaError, ExecaSyncError} from './lib/return/final-error.js';
 
 export const execa = createExeca(() => ({}));
 export const execaSync = createExeca(() => ({isSync: true}));
---
+++ test/methods/main-async.js
@@ -0,0 +1,26 @@
+import process from 'node:process';
+import test from 'ava';
+import {execa} from '../../index.js';
+import {setFixtureDir} from '../helpers/fixtures-dir.js';
+
+setFixtureDir();
+
+const isWindows = process.platform === 'win32';
+
+if (isWindows) {
+	test('execa() - cmd file', async t => {
+		const {stdout} = await execa('hello.cmd');
+		t.is(stdout, 'Hello World');
+	});
+
+	test('execa() - run cmd command', async t => {
+		const {stdout} = await execa('cmd', ['/c', 'hello.cmd']);
+		t.is(stdout, 'Hello World');
+	});
+}
+
+test('execa() returns a promise with pid', async t => {
+	const subprocess = execa('noop.js', ['foo']);
+	t.is(typeof subprocess.pid, 'number');
+	await subprocess;
+});

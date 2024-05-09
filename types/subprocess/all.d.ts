cf70951aefa31d849d6433a0f8f96f9ced5dfc96
ehmicky <ehmicky@users.noreply.github.com>
2024-05-09 19:06:40 +0100

    Reduce type complexity (#1038)

--- types/subprocess/all.d.ts
+++ types/subprocess/all.d.ts
@@ -3,22 +3,15 @@ import type {IgnoresSubprocessOutput} from '../return/ignore.js';
 import type {Options} from '../arguments/options.js';
 
 // `subprocess.all`
-export type SubprocessAll<OptionsType extends Options = Options> = AllStream<OptionsType['all'], OptionsType>;
+export type SubprocessAll<OptionsType extends Options = Options> = AllStream<AllIgnored<OptionsType['all'], OptionsType>>;
 
-type AllStream<
+type AllStream<IsIgnored> = IsIgnored extends true ? undefined : Readable;
+
+type AllIgnored<
 	AllOption extends Options['all'] = Options['all'],
 	OptionsType extends Options = Options,
 > = AllOption extends true
-	? AllIfStdout<IgnoresSubprocessOutput<'1', OptionsType>, OptionsType>
-	: undefined;
-
-type AllIfStdout<
-	StdoutResultIgnored extends boolean,
-	OptionsType extends Options = Options,
-> = StdoutResultIgnored extends true
-	? AllIfStderr<IgnoresSubprocessOutput<'2', OptionsType>>
-	: Readable;
-
-type AllIfStderr<StderrResultIgnored extends boolean> = StderrResultIgnored extends true
-	? undefined
-	: Readable;
+	? IgnoresSubprocessOutput<'1', OptionsType> extends true
+		? IgnoresSubprocessOutput<'2', OptionsType>
+		: false
+	: true;

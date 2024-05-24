c6c66dade58cf8ea533686c8c60bf3e597b5d66b
ehmicky <ehmicky@users.noreply.github.com>
2024-05-24 22:47:36 +0100

    Add tests for I/O errors during `sendMessage()` (#1095)

--- test/helpers/ipc.js
+++ test/helpers/ipc.js
@@ -1,4 +1,5 @@
 import {getEachMessage} from '../../index.js';
+import {foobarString} from './input.js';
 
 // @todo: replace with Array.fromAsync(subprocess.getEachMessage()) after dropping support for Node <22.0.0
 export const iterateAllMessages = async subprocess => {
@@ -25,3 +26,15 @@ export const getFirst = async () => {
 export const subprocessGetOne = (subprocess, options) => subprocess.getOneMessage(options);
 
 export const alwaysPass = () => true;
+
+// `process.send()` can fail due to I/O errors.
+// However, I/O errors are seldom and hard to trigger predictably.
+// So we mock them.
+export const mockSendIoError = anyProcess => {
+	const error = new Error(foobarString);
+	anyProcess.send = () => {
+		throw error;
+	};
+
+	return error;
+};

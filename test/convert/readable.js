d775ae558cfc0fa1d742eab7e4d0c7430b1e0056
ehmicky <ehmicky@gmail.com>
2024-07-21 23:37:30 +0100

    Fix stream-related tests with Node 22.5.0

--- test/convert/readable.js
+++ test/convert/readable.js
@@ -232,10 +232,11 @@ test('.readable() can pipe to errored stream with Stream.pipeline()', async t =>
 	outputStream.destroy(cause);
 
 	await assertPromiseError(t, pipeline(stream, outputStream), cause);
+	await t.throwsAsync(finishedStream(stream));
 
-	const error = await assertStreamError(t, stream, {cause});
+	const error = await assertStreamError(t, stream, cause);
 	await assertStreamReadError(t, outputStream, cause);
-	await assertSubprocessError(t, subprocess, error);
+	await assertSubprocessError(t, subprocess, {cause: error});
 });
 
 test('.readable() can be used with Stream.compose()', async t => {

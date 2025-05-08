1ac5b91eaaed89cc8b6a3d123e1af42ed65f9c33
ehmicky <ehmicky@users.noreply.github.com>
2025-05-08 21:09:38 +0100

    Fix Node 24-specific deprecation warning (#1199)

--- test/helpers/fixtures-directory.js
+++ test/helpers/fixtures-directory.js
@@ -1,7 +1,17 @@
 import path from 'node:path';
 import process from 'node:process';
 import {fileURLToPath} from 'node:url';
+import logProcessErrors from 'log-process-errors';
 import pathKey from 'path-key';
+
+// Make tests fail if any warning (such as a deprecation warning) is emitted
+logProcessErrors({
+	onError(error, event) {
+		if (event === 'warning') {
+			throw error;
+		}
+	},
+});
 
 export const PATH_KEY = pathKey();
 export const FIXTURES_DIRECTORY_URL = new URL('../fixtures/', import.meta.url);
